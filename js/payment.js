c44e455f8b6a4e3f35775ea0f3c1a5a46a0c0710
MinhTri30524 <triminh3005@gmail.com>
2025-08-13 10:28:38 +0700

    upup

--- js/payment.js
+++ js/payment.js
@@ -45,3 +45,22 @@ if (bar) {
    })
  }
 }
+
+
+const checkoutBtn = document.getElementById('checkoutBtn');
+const popup = document.getElementById('successPopup');
+const closePopup = document.getElementById('closePopup');
+
+checkoutBtn.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
+
+closePopup.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.style.display = 'none';
+  }
+});
